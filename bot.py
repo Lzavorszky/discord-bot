99203a8996599dbd110cda99cd62644bc134d158
lzavorszky <zavorszky_lorinc.co.uk>
2026-05-15 10:09:46 +0200

    connect OpenAI

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import os
 import discord
+from openai import OpenAI
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,19 +14,31 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f"Logged in as {client.user}")
 
 def ask_ai(question):
-    return "You said: " + question + " cefepim"
+    response = openai_client.responses.create(
+        model="gpt-5.4-mini",
+        input=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant. Keep answers short and clear."
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+        ],
+    )
+
+    return response.output_text
 
 @client.event
 async def on_message(message):
-
     if message.author == client.user:
         return
 
     answer = ask_ai(message.content)
-
     await message.channel.send(answer)
 
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
